3d7e7057c52f47a2a9398982a6ae62f150629143
Lee <liam.eakins@outlook.com>
2018-12-20 04:19:09 -0600

    Create contacts list

---
+++ js/contacts.js
@@ -0,0 +1,20 @@
+let peeps = [
+    {
+        name: "Mercury Man",
+        email: "theman@mercury.com",
+        tel: "+05551234",
+        loc: [46001200, 69816900]
+    },
+    {
+        name: "Venus 'Venturer",
+        email: "venus@theadventurer.com",
+        tel: "+15551234",
+        loc: [107477000, 108939000]
+    },
+    {
+        name: "Jupiter Juggernaut",
+        email: "thejuggler@jupiter.com",
+        tel: "+55551234",
+        loc: [816040000, 740550000]
+    }
+];
